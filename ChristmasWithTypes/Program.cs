df99abb3ba99feb67e9d510fdde2d2ef3fb98a84
David Chuvik <dchuvik@gmail.com>
2022-05-27 15:57:15 -0400

    fixed the spelling error

--- ChristmasWithTypes/Program.cs
+++ ChristmasWithTypes/Program.cs
@@ -14,7 +14,7 @@ namespace ChristmasWithTypes
             xmas.Santa = "Kris Kringle";
 
             //TODO Insert 3 presents you would like for xmas.  They must be strings
-            xmas.Presents =  new string[3]{ "a nice develpoer job", "to hangout with Jeremy", "my beard to connect"}; 
+            xmas.Presents =  new string[3]{ "a nice developer job", "to hangout with Jeremy", "my beard to connect"}; 
 
             //TODO Set the TreeHeight to 10
             xmas.TreeHeight = 10;
